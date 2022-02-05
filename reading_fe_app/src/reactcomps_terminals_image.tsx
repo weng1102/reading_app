37c571d616ca1ceab1ce4504a3618aa8584f053b
weng1102 <weng1102@gmail.com>
2022-02-05 12:41:12 -0800

    Added imageEntry parsing, changed references to use settingsContext

--- reading_fe_app/src/reactcomps_terminals_image.tsx
+++ reading_fe_app/src/reactcomps_terminals_image.tsx
@@ -3,27 +3,32 @@
  * File name: reactcomps_terminal_image.tsx
  *
  * Defines React front end functional components.
- * renders US phone numbers
+ * renders imageClasses
  *
  * Version history:
  *
  **/
-import React from "react";
+import React, { useContext } from "react";
 import { useAppSelector } from "./hooks";
 
 import { ITerminalInfo, IImageTerminalMeta } from "./pageContentType";
 import { TerminalNode, ITerminalPropsType } from "./reactcomps_terminals";
-
+import { ISettingsContext, SettingsContext } from "./settingsContext";
 export const Terminal_Image = React.memo((props: ITerminalPropsType): any => {
   const currentTerminalIdx = useAppSelector(store => store.cursor_terminalIdx); // causes rerendering
+  let settingsContext: ISettingsContext = useContext(
+    SettingsContext
+  ) as ISettingsContext;
+  let distDir = settingsContext.settings.config.distDir;
   console.log(
     `<Terminal_Image active=${props.active} content=${props.terminal.content}/>`
   );
   let imageInfo: IImageTerminalMeta = props.terminal.meta as IImageTerminalMeta;
-  let path: string = `https://weng1102.github.io/reading_app/dist/img/${imageInfo.src}`;
+  let path: string = `${distDir}/img/${imageInfo.src}`;
+  let imageClasses: string = "imageentry-image " + imageInfo.className;
   return (
     <>
-      <img className="image" src={path} alt={imageInfo.label} />
+      <img className={imageClasses} src={path} alt={imageInfo.label} />
     </>
   );
 });
