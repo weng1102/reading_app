333456345f83d89dfa653a72aa4b797bca222c2e
weng1102 <weng1102@gmail.com>
2022-02-05 13:38:50 -0800

    add help, dump linkList table, nooutput option

--- src/parsertest01.ts
+++ src/parsertest01.ts
@@ -1,9 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
+import assert from "assert";
 //import * as fs from "fs";
 import { ParseNodeSerializeFormatEnumType } from "./baseClasses";
 import { Logger } from "./logger";
 //import { FileParseNode } from "./parsefiles";
+import { AppNode } from "./parseapp";
 import { PageParseNode } from "./parsepages";
 import { IPageContent } from "./pageContentType";
 ///PageParseNode;
@@ -19,22 +21,41 @@ const inputPath: string = "curriculum/";
 const outputPath: string = "dist/";
 const inputExtension: string = ".md"; // markdown
 const outputExtension: string = ".json";
-
-let logger = new Logger(this);
-///let pageNode = new PageContent(this);
-//let timestamp: string = new MyDate().yyyymmddhhmmss();
-//let userRonlyn = new UserContext("Ronlyn");
-logger.verboseMode = true;
-logger.adornMode = true;
-//logger.diagnosticMode = true;
-//
-logger.info(
-  `verbose mode: ${logger.verboseMode ? "ON" : "OFF"}`,
-  false,
-  false,
-  false,
-  false
-);
+let appNode: AppNode = new AppNode();
+let logger: Logger = appNode.logger;
+appNode.logger.appName = "parsetest01";
+let inputFileSpecs: string[];
+let switches: string[] = [];
+let args: string[] = process.argv.slice(2);
+while (args.length > 0 && args[0].length > 0 && args[0][0] === "-") {
+  switches.push(args.shift()!);
+}
+if (switches.includes("--help")) {
+  logger.info(
+    `Usage: node parsetest01.ts [OPTIONS] [FILE(s)]\n` +
+      `Parses markdown files into json output for reader\n\n` +
+      `Options:\n` +
+      `  --dumpjson       display generated json\n` +
+      `  --dumplinks      display link list\n` +
+      `  --dumpsections   display section list\n` +
+      `  --dumpsentences  display sentence list\n` +
+      `  --dumpterminals  display terminal list\n` +
+      `  --dumptree       display parse tree\n` +
+      `  --nooutput       do not generate output json file(s)\n` +
+      `  --testreload     reload generated file\n` +
+      `  --adorn          adorn output mode\n` +
+      `  --diagnostic     diagnostic output mode\n` +
+      `  --verbose        verbose output mode\n`,
+    false,
+    false,
+    false,
+    false
+  );
+  process.exit();
+}
+logger.adornMode = switches.includes("--adorn");
+logger.diagnosticMode = switches.includes("--diagnostic");
+logger.verboseMode = switches.includes("--verbose");
 logger.info(
   `adorn mode: ${logger.adornMode ? "ON" : "OFF"}`,
   false,
@@ -42,15 +63,23 @@ logger.info(
   false,
   false
 );
-let inputFileSpecs: string[];
-let switches: string[] = [];
-let args: string[] = process.argv.slice(2);
-while (args.length > 0 && args[0].length > 0 && args[0][0] === "-") {
-  switches.push(args.shift()!);
-}
+logger.info(
+  `diagnostic mode: ${logger.diagnosticMode ? "ON" : "OFF"}`,
+  false,
+  false,
+  false,
+  false
+);
+logger.info(
+  `verbose mode: ${logger.verboseMode ? "ON" : "OFF"}`,
+  false,
+  false,
+  false,
+  false
+);
 inputFileSpecs = args.filter(mdFiles => mdFiles.endsWith(inputExtension));
 for (let inputFileSpec of inputFileSpecs) {
-  let pageNode = new PageParseNode(this);
+  let pageNode = new PageParseNode(appNode);
   linesParsed = pageNode.dataSource.connect(inputFileSpec);
   // console.log(
   //   pageNode.dataSource.serialize(ParseNodeSerializeFormatEnumType.TABULAR)
@@ -82,7 +111,7 @@ for (let inputFileSpec of inputFileSpecs) {
   );
   pageNode.filename = `${fileName}${outputExtension}`;
   let outputFileSpec = `${outputPath}${fileName}${outputExtension}`;
-  if (switches.includes("-dumpdatasource")) {
+  if (switches.includes("--dumpdatasource")) {
     logger.info(
       `dumping datasource for ${path.basename(outputFileSpec)}`,
       false,
@@ -90,14 +119,15 @@ for (let inputFileSpec of inputFileSpecs) {
       false,
       false
     );
-    logger.info(pageNode.dataSource.serialize(ParseNodeSerializeFormatEnumType.TABULAR),
+    logger.info(
+      pageNode.dataSource.serialize(ParseNodeSerializeFormatEnumType.TABULAR),
       false,
       false,
       false,
       false
-    )
+    );
   }
-if (switches.includes("-dumptree")) {
+  if (switches.includes("--dumptree")) {
     logger.info(
       `dumping tree for ${path.basename(outputFileSpec)}`,
       false,
@@ -105,14 +135,15 @@ if (switches.includes("-dumptree")) {
       false,
       false
     );
-    logger.info(pageNode.serialize(ParseNodeSerializeFormatEnumType.TREEVIEW),
+    logger.info(
+      pageNode.serialize(ParseNodeSerializeFormatEnumType.TREEVIEW),
       false,
       false,
       false,
       false
-    )
+    );
   }
-  if (switches.includes("-dumpjson")) {
+  if (switches.includes("--dumpjson")) {
     logger.info(
       `dumping json for ${path.basename(outputFileSpec)}`,
       false,
@@ -120,14 +151,15 @@ if (switches.includes("-dumptree")) {
       false,
       false
     );
-    logger.info(pageNode.serialize(ParseNodeSerializeFormatEnumType.TABULAR),
+    logger.info(
+      pageNode.serialize(ParseNodeSerializeFormatEnumType.TABULAR),
       false,
       false,
       false,
       false
-    )
+    );
   }
-  if (switches.includes("-dumpterminals")) {
+  if (switches.includes("--dumpterminals")) {
     logger.info(
       `dumping terminal list for ${path.basename(outputFileSpec)}`,
       false,
@@ -135,14 +167,15 @@ if (switches.includes("-dumptree")) {
       false,
       false
     );
-    logger.info(pageNode.userContext.terminals.serialize(),
+    logger.info(
+      pageNode.userContext.terminals.serialize(),
       false,
       false,
       false,
       false
-    )
+    );
   }
-  if (switches.includes("-dumpsentences")) {
+  if (switches.includes("--dumpsentences")) {
     logger.info(
       `dumping sentence list for ${path.basename(outputFileSpec)}`,
       false,
@@ -150,14 +183,31 @@ if (switches.includes("-dumptree")) {
       false,
       false
     );
-    logger.info(pageNode.userContext.sentences.serialize(),
+    logger.info(
+      pageNode.userContext.sentences.serialize(),
       false,
       false,
       false,
       false
-    )
+    );
   }
-  if (switches.includes("-dumpsections")) {
+  if (switches.includes("--dumplinks")) {
+    logger.info(
+      `dumping links for ${path.basename(outputFileSpec)}`,
+      false,
+      false,
+      false,
+      false
+    );
+    logger.info(
+      pageNode.userContext.links.serialize(),
+      false,
+      false,
+      false,
+      false
+    );
+  }
+  if (switches.includes("--dumpsections")) {
     logger.info(
       `dumping section list for ${path.basename(outputFileSpec)}`,
       false,
@@ -165,26 +215,32 @@ if (switches.includes("-dumptree")) {
       false,
       false
     );
-    logger.info(pageNode.userContext.sections.serialize(),
+    logger.info(
+      pageNode.userContext.sections.serialize(),
       false,
       false,
       false,
       false
-    )
+    );
   }
-  logger.info(
-    `generating json file ${path.basename(outputFileSpec)}`,
-    false,
-    false,
-    false,
-    false
-  );
   let json: string = pageNode.serialize(ParseNodeSerializeFormatEnumType.JSON);
-  fs.writeFileSync(outputFileSpec, json);
-  if (switches.includes("-testreload")) {
-    let inputStr: string = fs.readFileSync(outputFileSpec).toString()
+  if (!switches.includes("--nooutput")) {
+    logger.info(
+      `generating json file ${path.basename(outputFileSpec)}`,
+      false,
+      false,
+      false,
+      false
+    );
+    fs.writeFileSync(outputFileSpec, json);
+  }
+  if (switches.includes("--testreload")) {
+    let inputStr: string = fs.readFileSync(outputFileSpec).toString();
     let reloaded: IPageContent = JSON.parse(inputStr);
-    if (reloaded !== pageNode) {
+    try {
+      assert.deepEqual(reloaded, pageNode);
+    } catch (e) {
+      console.log((<Error>e).message);
       logger.info(
         `integrity failed for file ${path.basename(outputFileSpec)}`,
         false,
