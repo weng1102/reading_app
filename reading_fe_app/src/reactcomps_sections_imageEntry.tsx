dc76859cb6110972c194cee9c28ecf3eda8d41e8
weng1102 <weng1102@gmail.com>
2022-02-05 14:09:36 -0800

    added imageentry rendering

--- reading_fe_app/src/reactcomps_sections_imageEntry.tsx
+++ reading_fe_app/src/reactcomps_sections_imageEntry.tsx
@@ -1,6 +1,6 @@
 /** Copyright (C) 2020 - 2021 Wen Eng - All Rights Reserved
  *
- * File name: reactcomps_sections.tsx
+ * File name: reactcomps_sections_imageEntry.tsx
  *
  * Defines React front end functional components.
  *
@@ -12,6 +12,7 @@ import React from "react";
 import "./App.css";
 import { useAppSelector, useDivRef } from "./hooks";
 import {
+  ImageEntryLayoutEnumType,
   ISectionContent,
   ISentenceContent,
   ISectionImageEntryVariant,
@@ -22,36 +23,47 @@ import {
   ITerminalInfo,
   IImageTerminalMeta
 } from "./pageContentType";
+import { Terminal_Image } from "./reactcomps_terminals_image";
 import { SectionDispatcher, ISectionPropsType } from "./reactcomps_sections";
 
 interface ISectionImageEntryImagesPropsType {
+  active: boolean;
   images: ITerminalContent[];
+  layout: ImageEntryLayoutEnumType;
 }
-interface ISectionImageEntryImagePropsType {
-  image: ITerminalContent;
-}
+// interface ISectionImageEntryImagePropsType {
+//   distDir: string;
+//   image: ITerminalContent;
+// }
 interface ISectionImageEntryCaptionsPropsType {
   active: boolean;
   captions: ISectionContent[];
-}
-interface ISectionImageEntryCaptionPropsType {
-  active: boolean;
-  caption: ISectionContent;
+  layout: ImageEntryLayoutEnumType;
 }
 export const Section_imageEntry = React.memo(
   (props: ISectionPropsType): any => {
     let imageEntry: ISectionImageEntryVariant = props.section
       .meta as ISectionImageEntryVariant;
     // let images1 = (props.section.meta as ISectionImageEntryVariant).images // not working with jsx!!!
+
     let images = imageEntry.images as ITerminalContent[];
     let captions = imageEntry.captions as ISectionContent[];
+    let className: string =
+      imageEntry.layout.charAt(0).toLowerCase() === "l"
+        ? "imageentry-container-" + ImageEntryLayoutEnumType.left.toString()
+        : "imageentry-container-" + ImageEntryLayoutEnumType.above.toString();
     return (
       <>
-        <div className="image-entry-horizontal-container">
-          <Section_imageEntry_images images={images} />
+        <div className={className}>
+          <Section_imageEntry_images
+            active={props.active}
+            images={images}
+            layout={imageEntry.layout}
+          />
           <Section_imageEntry_captions
             active={props.active}
             captions={captions}
+            layout={imageEntry.layout}
           />
         </div>
       </>
@@ -61,33 +73,47 @@ export const Section_imageEntry = React.memo(
 export const Section_imageEntry_images = React.memo(
   (props: ISectionImageEntryImagesPropsType): any => {
     let images: ITerminalContent[] = props.images;
+    let className: string = "imageentry-image-";
+    className +=
+      props.layout === ImageEntryLayoutEnumType.above
+        ? ImageEntryLayoutEnumType.above.toString()
+        : ImageEntryLayoutEnumType.left.toString();
+    images.forEach(
+      image => ((image.meta as IImageTerminalMeta).className = className)
+    );
     return (
       <>
-        <div className="image-entry-images">
+        <div className={className}>
           {images.map((image, keyvalue: number) => (
-            <Section_imageEntry_image image={image} />
+            <Terminal_Image active={props.active} terminal={image} />
           ))}
         </div>
       </>
     );
   }
 );
-export const Section_imageEntry_image = React.memo(
-  (props: ISectionImageEntryImagePropsType): any => {
-    let image: IImageTerminalMeta = props.image.meta as IImageTerminalMeta;
-    return (
-      <>
-        <img src={image.src} alt={image.label} />
-      </>
-    );
-  }
-);
+// export const Section_imageEntry_image = React.memo(
+//   (props: ISectionImageEntryImagePropsType): any => {
+//     let image: IImageTerminalMeta = props.image.meta as IImageTerminalMeta;
+//     let imgSrc: string = `${props.distDir}/${image.src}`;
+//     return (
+//       <>
+//         <img src={imgSrc} alt={image.label} />
+//       </>
+//     );
+//   }
+// );
 export const Section_imageEntry_captions = React.memo(
   (props: ISectionImageEntryCaptionsPropsType): any => {
     let captions: ISectionContent[] = props.captions;
+    let className: string = "imageentry-captions-";
+    className +=
+      props.layout === ImageEntryLayoutEnumType.above
+        ? ImageEntryLayoutEnumType.above.toString()
+        : ImageEntryLayoutEnumType.left.toString();
     return (
       <>
-        <div className="image-entry-captions">
+        <div className={className}>
           {captions.map((caption, keyvalue: number) => (
             <SectionDispatcher active={props.active} section={caption} />
           ))}
